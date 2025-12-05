e7ffcbb427f5d12205b29ed85d6fa13ec3ad433f
kairon27 <stanislavchuk.yurii@gmail.com>
2025-12-06 01:05:41 +0200

    Create anim_banner.js

---
+++ anim_banner.js
@@ -0,0 +1,5 @@
+document.write(
+  '<a href="https://amopublisher.com/journals/?utm_source=Ejmhr&utm_medium=Banner&utm_campaign=Animate" target="_blank" rel="noopener">' +
+    '<img src="https://amopublisher.com/wp-content/uploads/2025/12/banner_up_06_12_2025.gif" alt="Banner">' +
+  '</a>'
+);
